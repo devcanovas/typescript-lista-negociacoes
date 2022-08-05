c353b8a38f924029da5748fee15c276808a42d7f
Matheus Canovas <matheuscanovas9@gmail.com>
2022-08-05 15:29:08 -0300

    Refatorando o código

--- app/models/lista-negociacoes.ts
+++ app/models/lista-negociacoes.ts
@@ -2,6 +2,9 @@ import { Negociacao } from './negociacao.js';
 
 export class ListaNegociacoes {
 
+    // também pode ser feito dessa forma:
+    // private listaNegociacoes: Negociacao[] = [];
+    // porém da forma abaixo é muita mais fácil de entender
     private listaNegociacoes: Array<Negociacao> = [];
 
 
@@ -9,6 +12,9 @@ export class ListaNegociacoes {
         this.listaNegociacoes.push(negociacao);
     }
 
+    //também pode ser feito dessa forma:
+    // public showAll(): readonly Negociacao[] {}
+    // agora o readonly passa a ser um modificador de acesso, e o array é declarado como no exemplo anterior
     public showAll(): ReadonlyArray<Negociacao> {
         return this.listaNegociacoes;
     }
--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,13 @@
 export class Negociacao { 
 
-    private _data: Date;
+    constructor(
+        private _data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number
+        ) {}
 
-    private _valor: number;
-
-    private _quantidade: number;
-
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }   
-
-    public get data(): Date {
-        return this._data;
-    }
-
-    public get quantidade(): number {
-        return this._quantidade;
-    }
-
-    public get valor(): number {
-        return this._valor;
+    get data(): Date {
+        return new Date(this._data.getTime());
     }
 
 }
